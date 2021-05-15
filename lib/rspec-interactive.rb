010af92290706e07d0e771a4a5a19effa60b9849
Nick Dower <nicholasdower@gmail.com>
2021-05-15 07:05:19 +0200

    Switch from custom REPL to Pry.

--- lib/rspec-interactive.rb
+++ lib/rspec-interactive.rb
@@ -3,26 +3,20 @@
 require 'json'
 require 'readline'
 require 'rspec/core'
-require 'shellwords'
 require 'pry'
 
 require 'rspec-interactive/runner'
 require 'rspec-interactive/config_cache'
+require 'rspec-interactive/input_completer'
 
 module RSpecInteractive
   class Console
 
     HISTORY_FILE = '.rspec_interactive_history'.freeze
     CONFIG_FILE = '.rspec_interactive_config'.freeze
-    MAX_HISTORY_ITEMS = 100
-    COMMANDS = ['help', 'rspec', 'pry', 'exit']
 
-    def initialize(config, stty_save)
-      @config = config
-      @stty_save = stty_save
-      @mutex = Mutex.new
-      @runner = nil
-      @config_cache = RSpecInteractive::ConfigCache.new
+    class <<self
+      attr_accessor :config, :stty_save, :mutex, :runner, :config_cache
     end
 
     def self.start(args)
@@ -31,24 +25,49 @@ module RSpecInteractive
         exit!(1)
       end
 
-      config = get_config(args[0])
-      stty_save = %x`stty -g`.chomp
+      @config = get_config(args[0])
+      @stty_save = %x`stty -g`.chomp
+      @mutex = Mutex.new
+      @runner = nil
+      @config_cache = RSpecInteractive::ConfigCache.new
 
-      Console.new(config, stty_save).start
-    end
-
-    def start
       load_rspec_config
       check_rails
-      load_history
-      configure_auto_complete
       trap_interrupt
-      start_console
+      configure_pry
+
+      Pry::Commands.create_command "rspec" do
+        description "Invoke RSpec."
+
+        banner <<-BANNER
+          Usage: rspec [arguments]
+
+          See https://relishapp.com/rspec/rspec-core/docs/command-line.
+        BANNER
+
+        def process
+          parsed_args = args.flat_map do |arg|
+            if arg.match(/[\*\?\[]/)
+              glob = Dir.glob(arg)
+              glob.empty? ? [arg] : glob
+            else
+              [arg]
+            end
+          end
+
+          runner = RSpecInteractive::Runner.new(parsed_args)
+          RSpecInteractive::Console.runner = runner
+          runner.run
+          RSpecInteractive::Console.runner = nil
+          RSpec.reset
+          RSpecInteractive::Console.config_cache.replay_configuration
+        end
+      end
+
+      Pry.start
     end
 
-    private
-
-    def check_rails
+    def self.check_rails
       if defined?(Rails)
         if Rails.application.config.cache_classes
           STDERR.puts "warning: Rails.application.config.cache_classes enabled. Disable to ensure code is reloaded."
@@ -56,11 +75,11 @@ module RSpecInteractive
       end
     end
 
-    def load_rspec_config
+    def self.load_rspec_config
       @config_cache.record_configuration(&rspec_configuration)
     end
 
-    def rspec_configuration
+    def self.rspec_configuration
       proc do
         if @config["init_script"]
           $LOAD_PATH << '.'
@@ -71,7 +90,7 @@ module RSpecInteractive
 
     def self.get_config(name = nil)
       unless File.exists? CONFIG_FILE
-        STDERR.puts "WARNING: using default config, file not found: #{CONFIG_FILE}"
+        STDERR.puts "warning: #{CONFIG_FILE} not found, using default config"
         return {}
       end
 
@@ -109,7 +128,7 @@ module RSpecInteractive
       end
     end
 
-    def trap_interrupt
+    def self.trap_interrupt
       trap('INT') do
         if @runner
           # We are on a different thread. There is a race here. Ignore nil.
@@ -122,129 +141,17 @@ module RSpecInteractive
       end
     end
 
-    def load_history
-      if File.exists? HISTORY_FILE
-        lines = File.readlines(HISTORY_FILE)
-        lines.each do |line|
-          Readline::HISTORY << line.strip
-        end
-      end
-    end
-
-    def configure_auto_complete
-      Readline.completion_append_character = ""
-    end
-
-    def start_console
-      loop do
-        buffer = Readline.readline('> ', true)&.strip
-
-        # Exit on ctrl-D.
-        if !buffer
-          puts
-          system "stty", @stty_save
-          exit!(0)
-        end
-
-        # Ignore blank lines.
-        if buffer.empty?
-          Readline::HISTORY.pop
-          next
-        end
-
-        # Write history to file.
-        if Readline::HISTORY.size > 0
-          file = File.open(HISTORY_FILE, 'w')
-          lines = Readline::HISTORY.to_a
-          lines[-[MAX_HISTORY_ITEMS, lines.size].min..-1].each do |line|
-            file.write(line.strip + "\n")
-          end
-          file.close
-        end
-
-        # Handle quoting, etc.
-        args = Shellwords.shellsplit(buffer)
-        next if args.empty?
-
-        command = args[0].strip
-        if COMMANDS.include?(command)
-          send "command_#{command}".to_sym, args[1..-1]
-        else
-          STDERR.puts "command not found: #{args[0]}"
-        end
-      end
-    end
-
-    def command_help(args)
-      if !args.empty?
-        STDERR.puts "invalid argument(s): #{args}"
-        return
-      end
-
-      print "commands:\n\n"
-      print "help  - print this message\n"
-      print "rspec - execute the specified spec file(s), wildcards allowed\n"
-      print "pry   - start pry\n"
-      print "exit  - exit\n"
-    end
-
-    def command_rspec(args)
-      parsed_args = args.flat_map do |arg|
-        if arg.match(/[\*\?\[]/)
-          glob = Dir.glob(arg)
-          glob.empty? ? [arg] : glob
-        else
-          [arg]
-        end
-      end
-
+    def self.configure_pry
       # Prevent Pry from trapping too. It will break ctrl-c handling.
       Pry.config.should_trap_interrupts = false
 
-      # Set Pry to use Readline, like us. This is the default anyway.
+      # Set Pry to use Readline. This is the default anyway.
       Pry.config.input = Readline
 
-      # If Pry does get used, it will add to history. We will clean that up after running the specs.
-      history_size = Readline::HISTORY.size
+      # Use custom completer to get file completion.
+      Pry.config.completer = RSpecInteractive::InputCompleter
 
-      # Initialize the runner. Also accessed by the signal handler above.
-      # RSpecInteractive::Runner sets RSpec.world.wants_to_quit to false. The signal
-      # handler sets it to true. 
-      @mutex.synchronize { @runner = RSpecInteractive::Runner.new(parsed_args) }
-
-      # Run the specs.
-      @runner.run
-
-      # Clear the runner.
-      @mutex.synchronize { @runner = nil }
-
-      while Readline::HISTORY.size > history_size do
-        Readline::HISTORY.pop
-      end
-
-      # Clear data from previous run.
-      RSpec.reset
-
-      @config_cache.replay_configuration
-    end
-
-    def command_pry(args)
-      if args.size != 0
-        STDERR.puts "unexpected argument(s): #{args}"
-        return
-      end
-
-      history_size = Readline::HISTORY.size
-      Pry.config.input = Readline
-      Pry.config.should_trap_interrupts = false
-      Pry.start
-      while Readline::HISTORY.size > history_size do
-        Readline::HISTORY.pop
-      end
-    end
-
-    def command_exit(args)
-      exit!(0)
+      Pry.config.history_file = HISTORY_FILE
     end
   end
 end
